7310b2556734c7bf0e4297e771b945635eacf4bd
adhavan27 <105037638+adhavan27@users.noreply.github.com>
2022-09-30 05:14:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,21 +9,12 @@ app.set('port', (process.env.PORT || 9981))
 app.use(express.static(__dirname + '/images'))
 
 
-/*
-app.get("/getCall", function(req,res){ 
-console.log("GET Method caled");
-console.log(__dirname);
 
-res.send("<h2>Welcome to Node JS express app</h2>"+appEnv.url+appEnv.port+port+process.env.LOGNAME);
-
-}).listen(9009);
-console.log(__dirname+"/images/mithunlogo.jpg");
-*/
-app.get('/mithuntechnologies', function(request, response) {
+app.get('/Unimity', function(request, response) {
     //response.send("<h2><center>Welcome to Node JS app</h2>");
     response.write("<h2><center><u>Node JS  Application </u></center></h2>");
 	
-    response.write("<h2><center>Welcome to  Mithun Technologies. Please Contact +91-9980923226,++91-9980923216 for more information or send an email to devopstrainingblr@gmail.com <center></h2>" );
+    response.write("<h2><center>unimity assessment <center></h2>" );
     response.end();
     
   })
@@ -39,18 +30,6 @@ app.get("/html", function(req,res){
     res.end();
     
     });
-    app.get("/jsonData", function(req,res){
-        res.type('json');
-        //res.type('application/json');
-        //res.json({'name': 'Mithun Reddy L'});
-        res.send({
-		'name': 'Mithun Technologies',
-		'technology': 'DevOps',
-		'contact' : '9980923226',
-		'email': 'devopstrainingblr@gmail.com'
-	            });
-        
-        });
 app.get("/queryparam", function(req,res){
 //res.send(req.query);
 res.send(req.query.key + ": " + req.query.name);
@@ -68,14 +47,9 @@ app.get("/status-code-500", function(req, res) {
    res.status(500).send('Internal Server Error – custom message');
 })
 
-app.get('/redirect', function(req, res) {
-    //Send status 300
-        res.redirect('http://mithuntechnologies.com');
-    });
-    
  
     app.listen(app.get('port'), function() {
-        console.log("Node JS app is running at http://localhost:" + app.get('port') +"/mithuntechnologies");
+        console.log("Node JS app is running at http://localhost:" + app.get('port') +"/Unimity");
       })
     
 
